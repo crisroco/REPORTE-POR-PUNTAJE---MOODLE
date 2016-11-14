0172e3d00aaa28fef67a9aaa0a13f5e9ef6e613a
Sail3 <crc1992_3@hotmail.com>
2016-11-14 17:23:40 -0500

    separar por seccion styles

--- amd/src/module.js
+++ amd/src/module.js
@@ -11,3 +11,19 @@ define(['jquery','block_sync/select2'], function($){
     init: init,
   }
 });
+
+/*define(['jquery'], function($){
+
+    function init(){
+        $(document).ready(function(){
+            $('.collapse').click(function(e){
+                e.preventDefault();
+                var target = $(this).attr('target');
+                $(target).slideToggle();
+            });
+        });
+    }
+    
+
+
+});*/
