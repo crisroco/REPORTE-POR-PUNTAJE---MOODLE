01c4523b2b4c87ef0f3fb1d3f0f46fec9954735b
crisroco <crc1992_3@hotmail.com>
2017-04-12 00:45:37 -0500

    data completa reporte de participacion

--- reports.php
+++ reports.php
@@ -11,8 +11,6 @@ global $DB, $CFG, $PAGE, $OUTPUT, $USER;
 
 require_login();
 
-//$categoryid = $_GET['categoryid'];
-//$section_course = $_GET['section_course'];
 $categoryid = required_param('categoryid', PARAM_INT);
 $section_course = required_param('section_course', PARAM_INT);
 
@@ -62,7 +60,7 @@ $section_course = required_param('section_course', PARAM_INT);
 
 
    /**
-      devuelve las encuestas vencidas y de una semana especifica
+      devuelve datos de las actividades(encuesta y tareas)
    */
 
       //cm.instance -> es el id del modulo
@@ -73,26 +71,50 @@ $section_course = required_param('section_course', PARAM_INT);
          $type_activity = "SELECT cm.instance, cm.module
                            from {course_modules} as cm
                            INNER JOIN {course_sections} as cs ON cm.section = cs.id
-                           where cs.course = $value->course_id AND cs.section = $section_course ";
-         $tipo_actividad = $DB->get_records_sql($type_activity);
-         echo "<pre> #########";
+                           where cs.course = $value->course_id AND cs.section = $section_course AND (cm.module = 7 or cm.module = 1)";
+         //solo debe haber una encuesta o una tarea por semana
+         $tipo_actividad = $DB->get_record_sql($type_activity);
+        
+         /*echo "<pre> #########";
          print_r($tipo_actividad);
-         echo "</pre>#########"; 
-
+         echo "</pre>#########"; */
+         
+         if (is_object($tipo_actividad)) {
+            $activ = $tipo_actividad->module;
+         }
          //FLUJO DE FEEDBACK
-          $sql_feedback = "SELECT fb.id,  fb.name, fb.course, cs.section as semana , cm.module 
-                           from {feedback} as fb  
-                           join {course_modules} as cm ON fb.id = cm.instance
-                           join {course_sections} as cs ON cm.section = cs.id
-                           where fb.course= $value->course_id AND fb.timeclose < $now AND cs.section = $section_course" ;
+         if ($activ == 7) {
+            
+             $sql_feedback = "SELECT fb.id,  fb.name, fb.course, cs.section as semana , cm.module 
+                              from {feedback} as fb  
+                              join {course_modules} as cm ON fb.id = cm.instance
+                              join {course_sections} as cs ON cm.section = cs.id
+                              where fb.course= $value->course_id AND fb.timeclose < $now AND cs.section = $section_course" ;
 
-          $actividad = $DB->get_records_sql($sql_feedback);
-          if ($actividad != array()) {
-            foreach ($actividad as $llave => $valor) {
-               array_push($actividades, $valor);
-            }
-          }
-         //FIN - FLUJO DE FEEDBACK 
+             $actividad = $DB->get_records_sql($sql_feedback);
+             if ($actividad != array()) {
+               foreach ($actividad as $llave => $valor) {
+                  array_push($actividades, $valor);
+               }
+             }
+         //FIN - FLUJO DE FEEDBACK
+         //FLUJO DE TAREA 
+         }elseif ($activ == 1) {
+            $sql_assign = "SELECT ass.id,  ass.name, ass.course, cs.section as semana , cm.module 
+                              from {assign} as ass  
+                              join {course_modules} as cm ON ass.id = cm.instance
+                              join {course_sections} as cs ON cm.section = cs.id
+                              where ass.course= $value->course_id  AND cs.section = $section_course" ;
+
+             $actividad = $DB->get_records_sql($sql_assign);
+             if ($actividad != array()) {
+               foreach ($actividad as $llave => $valor) {
+                  array_push($actividades, $valor);
+               }
+             }
+         //FIN - FLUJO DE TAREA 
+         }
+         
       }
 
          echo "<pre>";
@@ -104,24 +126,84 @@ $section_course = required_param('section_course', PARAM_INT);
    */
       $all_data = array();
       foreach ($actividades as $key => $value) {
-         $puntaje = 0;
-         $sql_feedback_cantidad = "SELECT count(fc.id) as cantidad_participante from {feedback_completed} as fc where fc.feedback = $value->id";
-         $encuestas_cantidad = $DB->get_records_sql($sql_feedback_cantidad);
-         foreach ($encuestas_cantidad as $llave => $valor) {        
-           
-            $puntaje = ($valor->cantidad_participante/$cursos[$value->course]->students)*100;
-         }
-         $fb_name = $value->name;
+         if ($value->module == 7) {
+            $puntaje = 0;
+            $sql_feedback_cantidad = "SELECT count(fc.id) as cantidad_participante from {feedback_completed} as fc where fc.feedback = $value->id";
+            $encuestas_cantidad = $DB->get_records_sql($sql_feedback_cantidad);
+            foreach ($encuestas_cantidad as $llave => $valor) {        
+              
+               $puntaje = ($valor->cantidad_participante/$cursos[$value->course]->students)*100;
+            }
+            $fb_name = $value->name;
 
-         $datos_feedback = new stdClass();
-         $datos_feedback->activity_name = $fb_name;
-         $datos_feedback->tipo = 'Feedback';
-         $datos_feedback->puntaje = $puntaje;
-         array_push($all_data, $datos_feedback);
+            $datos_feedback = new stdClass();
+            $datos_feedback->activity_name = $fb_name;
+            $datos_feedback->tipo = 'Encuesta';
+            $datos_feedback->puntaje = $puntaje.'%';
+            array_push($all_data, $datos_feedback);
+
+         }elseif ($value->module == 1) {
+            $puntaje = 100;
+            $now = microtime(true);
+            $sql_tarea_verification = "SELECT subfl.submission, ass.id, ass.name, ass.allowsubmissionsfromdate  
+                                       from {assign} as ass
+                                       join {assignsubmission_file} as subfl ON ass.id = subfl.assignment 
+                                       where ass.id = $value->id";
+            $tarea_verification = $DB->get_records_sql($sql_tarea_verification);
+
+            while ( count($tarea_verification) > 1) {
+               array_pop($tarea_verification);
+            }
+
+            foreach ($tarea_verification as $key => $value) {
+
+               $time_upload = $value->allowsubmissionsfromdate;
+               $one_day = $time_upload+86400;
+               $two_day = $time_upload+86400;
+               $three_day = $time_upload+86400;
+               $four_day = $time_upload+86400;
+               $five_day = $time_upload+86400;
+
+               if ($time_upload < $now) {
+                  $puntaje = 100;
+               }elseif ($now == $one_day) {
+                  $puntaje = 80;
+               }elseif ($now == $one_day) {
+                  $puntaje = 60;                  
+               }elseif ($now == $one_day) {
+                  $puntaje = 40;                  
+               }elseif ($now == $one_day) {
+                  $puntaje = 20;                  
+               }elseif ($now == $one_day) {
+                  $puntaje = 0;                  
+               }   
+            
+
+               $ass_name = $value->name;
+
+               $datos_tarea = new stdClass();
+               $datos_tarea->activity_name = $ass_name;
+               $datos_tarea->tipo = 'Tarea';
+               $datos_tarea->puntaje = $puntaje;
+               array_push($all_data, $datos_tarea);
+            }
+            
+            //array_push($all_data, $tarea_verification);
+            
+         }
       }
 
       echo "<pre>";
       print_r($all_data);
       echo "</pre>";
+
+      $fecha = date_create();
+
+
+date_timestamp_set($fecha, 1491886800+86400+86400);
+echo date_format($fecha, 'U = Y-m-d H:i:s') . "<br>";
+
+date_timestamp_set($fecha, 1491886800);
+echo date_format($fecha, 'U = Y-m-d H:i:s') . "\n";
 
  
